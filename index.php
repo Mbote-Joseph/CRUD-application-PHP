484cbd0fc5ce46914ffc0509b1b0c94a73f9f2dd
Mbote-Joseph <mbotejoseph001@gmail.com>
2021-04-19 11:34:40 +0300

    Operations added

--- index.php
+++ index.php
@@ -4,32 +4,106 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
     <title>PHP CRUD application</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
 </head>
 <body>
 <div class="container">
+    
+    <?php require_once"server.php"; ?>
+
+    <?php  
+        if(isset($_SESSION['message'])) :
+    ?>
+    <div class="alert alert-<?php echo $_SESSION['msg_type']; ?>" >
+            <?php 
+            echo $_SESSION['message'];
+            unset($_SESSION['message']);
+            header("Refresh:1");
+             ?>
+    </div>
+
+    <?php endif; ?>
+    <?php 
+        $mysqli= new mysqli('localhost', 'root', '','crud') or die(mysqli_error($mysqli));
+        $result= $mysqli->query("SELECT * FROM Datatable") or die($mysqli->error);
+        //pre_r($result);
+        //pre_r($result->fetch_assoc());
+    ?>
+
+    <div class="row justify-content-center">
+    <table class="table">
+    <thead>
+    <tr>
+        
+        <th>name</th>
+        <th>email</th>
+        <th>phone</th>
+        <th>location</th>
+        <th>Registration Date</th>
+        <th colspan="2">Action</th>
+    </tr>
+    </thead>
+    
+    <?php
+        while($row=$result->fetch_assoc()): 
+    ?>
+    <tr>
+        <td><?php echo $row['name'];  ?></td>
+        <td><?php echo $row['email'];  ?></td>
+        <td><?php echo $row['phone'];  ?></td>
+        <td><?php echo $row['location'];  ?></td>
+        <td><?php echo $row['RegistrationDate']; ?></td>
+        <td>
+            <a href="index.php?edit=<?php echo $row['id']; ?>"  class="btn btn-info"> Edit</a>
+            <a href="server.php?delete=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
+        
+        </td>
+    </tr>
+    <?php endwhile; ?>
+    
+    </table>
+    
+    </div>
+
+        
+    <?php
+
+        function pre_r($array){
+            echo '<pre>';
+            print_r($array);
+            echo '</pre>';
+        }
+    ?>
     <div class="row justify-content-center my-5">
-    <form action="server.php" method="post" style="width: 500px;">
+    <form class="form-group" action="server.php" method="post" style="width: 500px;">
+    <input type="hidden" name="id" value="<?php echo $id ?>">
     <div class="form-group">
     <label for="name">Name</label>
-    <input class="form-control" type="text" name="name" placeholder="Enter your full name">
+    <input class="form-contdol" type="text" name="name" value="<?php echo $name; ?>" placeholder="Enter your full name" required>
     </div>
     <div class="form-group">
     <label for="email">Email</label>
-    <input class="form-control" type="email" name="email" placeholder="Enter your email">
+    <input class="form-contdol" type="email" name="email" value="<?php echo $email; ?>" placeholder="Enter your email" required>
     </div>
     <div class="form-group">
     <label for="phone">Phone</label>
-    <input class="form-control" type="text" name="phone" placeholder="Phone number">
+    <input class="form-contdol" type="text" name="phone" value="<?php echo $phone; ?>" placeholder="Phone number" required>
     </div>
     <div class="form-group">
     <label for="location">Location</label>
-    <input class="form-control" type="text" name="location" placeholder="Location">
+    <input class="form-contdol" type="text" name="location" value="<?php echo $location; ?>" placeholder="Location" required>
     </div>
     <br>
-
-    <button class="btn btn-primary" type="submit">Submit</button>
+    <?php
+        if($update==true) :
+    ?>
+    <button class="btn btn-info" type="submit" name="update">Update</button>
+    <?php else : ?>
+    <button class="btn btn-primary" type="submit" name="save">Submit</button>
+    <?php endif; ?>
 
     
     </form>
--- server.php
+++ server.php
@@ -1,8 +1,72 @@
 <?php
+ session_start();
+$mysqli= new mysqli('localhost', 'root', '','crud') or die(mysqli_error($mysqli));
+$id=0;
+$name='';
+$email='';
+$phone='';
+$location='';
+$update=false;
 
-$mysqli= new mysqli('localhost', 'root', '','crud') or die(mysqli_error($mysqli))
+
+if(isset($_POST['save'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $phone=$_POST['phone'];
+    $location=$_POST['location'];
 
 
+    $mysqli->query("INSERT INTO Datatable(name, email, phone,location) VALUES('$name','$email','$phone','$location')" ) or 
+     die($mysqli->error);
 
+
+    $_SESSION['message']="Record Created !";
+    $_SESSION['msg_type']="success";
+
+    header("location: index.php");
+}
+
+if(isset($_GET['delete'])){
+    $id=$_GET['delete'];
+    
+
+    $mysqli->query("DELETE FROM Datatable WHERE id=$id") or die($mysqli->error);
+
+    $_SESSION['message']="Record Deleted !";
+    $_SESSION['msg_type']="danger";
+    header("location: index.php");
+}
+
+if(isset($_GET['edit'])){
+    $id= $_GET['edit'];
+    $update=true;
+    $result =$mysqli->query("SELECT * FROM Datatable WHERE id=$id") or die($mysqli->error);
+
+    if(count($results->num_rows)){
+        $row= $result->fetch_array();
+        $name=$row['name'];
+        $email=$row['email'];
+        $phone=$row['phone'];
+        $location=$row['location'];
+
+    }
+}
+
+if(isset($_POST['update'])){
+    $id=$_POST['id'];
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $phone=$_POST['phone'];
+    $location=$_POST['location'];
+
+    $mysqli->query("UPDATE Datatable SET name='$name', email='$email', phone='$phone', location='$location' WHERE id=$id ") or die($mysqli->error);
+
+
+    $_SESSION['message']='Record have being updated !';
+    $_SESSION['msg_type']='info';
+    header("location: index.php");
+}
+
+//$result= $mysqli->query("SELECT * FROM Datatable") or die($mysqli->error);
 
 ?>
